c2c2e0e427167f815b87c716be8b5d02ad0884db
MelodicAlbuild <ralexdrum@gmail.com>
2023-03-08 18:19:05 -0600

    Create Lookup Table System

---
+++ LookupTableCore/LookupTable.cs
@@ -0,0 +1,53 @@
+﻿namespace MelodicAlbuild.Utils.LookupTable.Core;
+
+public class LookupTable
+{
+    private Dictionary<dynamic, dynamic> Table { get; }
+    private HashSet<dynamic> ReservedKeys { get; }
+
+    public LookupTable()
+    {
+        Table = new Dictionary<dynamic, dynamic>();
+        ReservedKeys = new HashSet<dynamic>();
+    }
+
+    public string Get(dynamic key)
+    {
+        return (ReservedKeys.Contains(key)
+            ? null
+            : Table[key])!;
+    }
+
+    public void Add(dynamic key, dynamic value)
+    {
+        if (ReservedKeys.Contains(key))
+        {
+            throw new InvalidOperationException("Key in Lookup Table is Reserved");
+        }
+
+        Table.Add(key, value);
+        ReservedKeys.Add(key);
+    }
+
+    public void Remove(dynamic key)
+    {
+        if (!ReservedKeys.Contains(key) || !Table.ContainsKey(key))
+        {
+            throw new InvalidOperationException("Key in Lookup Table does not exist.");
+        }
+
+        Table.Remove(key);
+        ReservedKeys.Remove(key);
+    }
+
+    public void Update(dynamic key, dynamic value)
+    {
+        if (!ReservedKeys.Contains(key) || !Table.ContainsKey(key))
+        {
+            throw new InvalidOperationException("Key in Lookup Table does not exist.");
+        }
+        
+        Remove(key);
+        Add(key, value);
+    }
+}
